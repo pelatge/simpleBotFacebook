60c3019a5e268afa3999eed62ddbaf74a7850064
pelatge <pelat.g29@gmail.com>
2022-03-01 03:58:38 +0700

    edit _error

--- _error.js
+++ _error.js
@@ -2,7 +2,7 @@ module.exports = async function HandleError(context, props) {
   console.error(props.error);
   // or you can choose not to reply any error messages
   await context.sendText(
-    'Ada masalah teknis. silahkan coba lagi, maaf atas ketidaknyamannya.'
+    'Ada masalah teknis nih. silahkan coba lagi, maaf atas ketidaknyamannya.'
   );
   if (process.env.NODE_ENV === 'production') {
     // send your error to your error tracker, for example: Sentry
